7311cf8234d5e8b49eb93dc226ed4e6391fe673c
emkis <nicolasemkis@gmail.com>
2023-05-31 00:17:40 +0100

    chore(next): setup vanilla extract

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
+
+const withVanillaExtract = createVanillaExtractPlugin({})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
 }
 
-export default nextConfig
+export default withVanillaExtract(nextConfig)
