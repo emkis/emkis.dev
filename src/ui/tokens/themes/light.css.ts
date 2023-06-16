2ee035f250c4d51df2abe4eca2d62ef7fab149b5
emkis <nicolasemkis@gmail.com>
2023-06-16 22:04:57 +0100

    feat(ui): add light and dark theme

---
+++ src/ui/tokens/themes/light.css.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@vanilla-extract/css'
+import { dynamicTokens } from '../dynamic.css'
+import { slate } from '@radix-ui/colors'
+
+export const lightTheme = createTheme(dynamicTokens, {
+  color: {
+    textPrimary: slate.slate12,
+    textSecondary: slate.slate11,
+    textLink: slate.slate12,
+    textLinkVisited: slate.slate12,
+    backgroundScreen: slate.slate1,
+    backgroundNeutral: slate.slate2,
+    backgroundElevated: slate.slate1,
+  },
+})
