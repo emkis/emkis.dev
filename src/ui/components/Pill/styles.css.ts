15ff766a94c3dae97babe8cc5457d0e98c804589
emkis <nicolasemkis@gmail.com>
2023-06-18 23:32:48 +0100

    refactor(ui): change fixed styles to tokens

--- src/ui/components/Pill/styles.css.ts
+++ src/ui/components/Pill/styles.css.ts
@@ -1,55 +1,42 @@
 import { style, styleVariants } from '@vanilla-extract/css'
-import { slate, slateDark } from '@radix-ui/colors'
+import { Tokens } from '@ui/tokens/index.css'
+import { lightTheme } from '@ui/tokens/themes/light.css'
 
 const root = style({
-  padding: '6px 12px',
+  padding: `${Tokens.padding[100]} ${Tokens.padding[200]}`,
   display: 'inline-block',
   lineHeight: 1,
   border: 0,
-  borderRadius: '32px',
+  borderRadius: Tokens.radius[400],
   transitionProperty: 'color, background-color',
   transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
   transitionDuration: '200ms',
 })
 
-const backgroundDefault = style({
-  background: slateDark.slate3,
-  ':hover': {
-    background: slateDark.slate4,
-  },
-  ':focus': {
-    background: slateDark.slate4,
-  },
-  ':active': {
-    background: slateDark.slate5,
-  },
-})
-
-const backgroundActive = style({
-  background: slate.slate3,
-  ':hover': {
-    background: slate.slate4,
-  },
-  ':focus': {
-    background: slate.slate4,
+const background = style({
+  background: Tokens.color.interactive,
+  selectors: {
+    '&:hover, &:focus': {
+      background: Tokens.color.interactiveHover,
+    },
   },
   ':active': {
-    background: slate.slate5,
+    background: Tokens.color.interactiveActive,
   },
 })
 
 export const backgroundVariant = styleVariants({
-  default: [root, backgroundDefault],
-  active: [root, backgroundActive],
+  default: [root, background],
+  active: [lightTheme, root, background],
 })
 
 export const textVariant = styleVariants({
   default: {
-    color: `${slateDark.slate11} !important`,
+    color: `${Tokens.color.textSecondary} !important`,
     lineHeight: '1 !important',
   },
   active: {
-    color: `${slate.slate12} !important`,
+    color: `${Tokens.color.textPrimary} !important`,
     lineHeight: '1 !important',
   },
 })
