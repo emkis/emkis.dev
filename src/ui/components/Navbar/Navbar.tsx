13882c55795abade99fc4da3d3d7961aae749898
emkis <nicolasemkis@gmail.com>
2023-06-23 21:52:59 +0100

    feat(ui): add placeholder for navbar

---
+++ src/ui/components/Navbar/Navbar.tsx
@@ -0,0 +1,5 @@
+import * as styles from './styles.css'
+
+export function Navbar() {
+  return <nav className={styles.navbar} />
+}
---
+++ src/ui/components/Navbar/styles.css.ts
@@ -0,0 +1,8 @@
+import { style } from '@vanilla-extract/css'
+
+export const navbar = style({
+  maxWidth: '1280px',
+  margin: '0 auto',
+  background: '#495841',
+  height: '68px',
+})
