4a055bf45cafe67694cf614cd734580205f18493
emkis <nicolasemkis@gmail.com>
2023-06-25 23:24:18 +0100

    delete unused file

--- src/ui/core/px-to-rem.ts
+++
@@ -1,9 +0,0 @@
-export type RemToken = `${string}rem`
-
-export function pxToRem(px: number): RemToken {
-  const baseFontSize = 16
-  const remSize = px / baseFontSize
-  const size = remSize.toFixed(2)
-
-  return `${size}rem` as const
-}
