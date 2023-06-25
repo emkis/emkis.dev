2761fe6599ae38bcdbe83aa35c008e653d42edd8
emkis <nicolasemkis@gmail.com>
2023-06-25 23:21:10 +0100

    feat(ui): add Navbar component

--- src/ui/components/Navbar/Navbar.tsx
+++ src/ui/components/Navbar/Navbar.tsx
@@ -1,5 +1,41 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+import { Text } from '@ui/components/Text'
+import { Pill } from '@ui/components/Pill'
+import { Link } from '@ui/components/Link'
 import * as styles from './styles.css'
 
 export function Navbar() {
-  return <nav className={styles.navbar} />
+  const pathname = usePathname()
+  const isHomeActive = pathname === '/'
+  const isArticlesActive = pathname.startsWith('/articles')
+  const isMeActive = pathname.startsWith('/me')
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.content}>
+        <Text as={Link} unstyled href="/" weight="bold" size="large" color="primary">
+          emkis.dev
+        </Text>
+
+        <div className={styles.links}>
+          <Pill as={Link} unstyled href="/" variant={isHomeActive ? 'active' : 'default'}>
+            Home
+          </Pill>
+          <Pill
+            as={Link}
+            unstyled
+            href="/articles"
+            variant={isArticlesActive ? 'active' : 'default'}
+          >
+            Articles
+          </Pill>
+          <Pill as={Link} unstyled href="/me" variant={isMeActive ? 'active' : 'default'}>
+            About me
+          </Pill>
+        </div>
+      </div>
+    </nav>
+  )
 }
--- src/ui/components/Navbar/styles.css.ts
+++ src/ui/components/Navbar/styles.css.ts
@@ -1,8 +1,27 @@
-import { style } from '@vanilla-extract/css'
+import { style, createVar, fallbackVar } from '@vanilla-extract/css'
+import { token } from '@tokens'
+
+export const navbarBackground = createVar()
 
 export const navbar = style({
   maxWidth: '1280px',
+  display: 'flex',
+  justifyContent: 'center',
   margin: '0 auto',
-  background: '#495841',
+  background: fallbackVar(navbarBackground, token('color.background-screen')),
   height: '68px',
+})
+
+export const content = style({
+  maxWidth: '1088px',
+  flex: '1',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  fontSize: token('font-size.250'),
+})
+
+export const links = style({
+  display: 'flex',
+  gap: token('spacing.200'),
 })
