91659f1e76a3f68c0d36f12cc8e6ef3575e869c9
emkis <nicolasemkis@gmail.com>
2023-06-25 01:49:30 +0100

    feat(app): add app styles

---
+++ src/app/app.css.ts
@@ -0,0 +1,6 @@
+import { style } from '@vanilla-extract/css'
+import { token } from '@tokens'
+
+export const app = style({
+  background: token('color.background-screen'),
+})
--- src/app/styles.css.ts
+++
@@ -1,6 +0,0 @@
-import { style } from '@vanilla-extract/css'
-import { Tokens } from '@ui/tokens/index.css'
-
-export const app = style({
-  background: Tokens.color.backgroundScreen,
-})
