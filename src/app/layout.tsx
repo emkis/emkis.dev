c04587dcfe128246a07025e8a5cbf1a2f73914b8
emkis <nicolasemkis@gmail.com>
2023-06-25 19:24:15 +0100

    feat: add app base metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,20 @@ import '@tokens/styles/theme-light.css'
 import '@tokens/styles/theme-dark.css'
 
 import type * as React from 'react'
+import type { Metadata } from 'next'
 import { registerTheme } from '@tokens'
 import { font } from '@tokens/font'
 import { cn } from '@ui/core'
 import * as styles from './app.css'
 
 type RootLayoutProps = { children: React.ReactNode }
+
+export const metadata: Metadata = {
+  title: {
+    default: 'emkis.dev',
+    template: '%s — emkis.dev',
+  },
+}
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
