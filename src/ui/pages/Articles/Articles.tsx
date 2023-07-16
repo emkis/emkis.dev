3d8748a3e7c77cf542274082f819ff4c2f8a41ab
emkis <nicolasemkis@gmail.com>
2023-07-17 00:33:10 +0100

    refactor(ui): remove unused colors

--- src/ui/pages/Articles/Articles.tsx
+++ src/ui/pages/Articles/Articles.tsx
@@ -7,18 +7,6 @@ import { Heading } from '@ui/components/Heading'
 import { Text } from '@ui/components/Text'
 import { ArticleCard } from '@ui/components/ArticleCard'
 import * as styles from './styles.css'
-import {
-  tomatoDark,
-  crimsonDark,
-  plumDark,
-  violetDark,
-  indigoDark,
-  grassDark,
-  orangeDark,
-  brownDark,
-  bronzeDark,
-  goldDark,
-} from '@radix-ui/colors'
 
 export const metadata: Metadata = {
   title: 'Articles',
@@ -27,18 +15,6 @@ export const metadata: Metadata = {
 
 export function Articles() {
   const articleColors = ['#5C3F3F', '#482D40', '#364A49', '#3E4A36', '#2D3148', '#48482D']
-  const _articleColors = [
-    tomatoDark.tomato5,
-    crimsonDark.crimson5,
-    plumDark.plum5,
-    violetDark.violet5,
-    indigoDark.indigo5,
-    grassDark.grass5,
-    orangeDark.orange5,
-    brownDark.brown5,
-    bronzeDark.bronze5,
-    goldDark.gold5,
-  ]
 
   const articleTitles = [
     'Moving state outside React',
