46218a4724e93c4665b662ff9e6b99179ef0bade
emkis <nicolasemkis@gmail.com>
2023-07-16 23:56:22 +0100

    feat(ui): add stable version of Article page

--- src/ui/pages/Article/Article.tsx
+++ src/ui/pages/Article/Article.tsx
@@ -1,24 +1,10 @@
 import * as React from 'react'
-import type { Metadata, ResolvingMetadata } from 'next'
-import Image from 'next/image'
-import { resolve } from 'node:path'
-import { readFileSync, readdirSync } from 'node:fs'
-import shiki from 'shiki'
-import { unified } from 'unified'
-import remarkParse from 'remark-parse'
-import remarkRehype from 'remark-rehype'
-import remarkShiki from '@stefanprobst/remark-shiki'
-import remarkCodeTitle from 'remark-code-title'
-import rehypeStringify from 'rehype-stringify'
-import rehypeSlug from 'rehype-slug'
-import rehypeRaw from 'rehype-raw'
-import rehypeReact from 'rehype-react'
-import { Text } from '@ui/components/Text'
+import type { Metadata } from 'next'
+import { getAllArticles, getArticleBySlug } from '@article'
+import { MDXToContent } from '@ui/core'
 import { Navbar } from '@ui/components/Navbar'
-import { Heading } from '@ui/components/Heading'
-import { Link } from '@ui/components/Link'
+import { ArticleHeader } from './ArticleHeader'
 import * as styles from './styles.css'
-import { slateDark } from '@radix-ui/colors'
 
 type PageProps = {
   params: {
@@ -26,106 +12,29 @@ type PageProps = {
   }
 }
 
-const writingsFolderPath = resolve(process.cwd(), 'writings')
-
 export async function generateStaticParams() {
-  const articleFiles: string[] = readdirSync(writingsFolderPath)
-
-  return articleFiles.map((fileName) => {
-    return { slug: fileName.replaceAll('.md', '') }
-  })
+  const articles = getAllArticles()
+  return articles.map((article) => ({ slug: article._raw.flattenedPath }))
 }
 
-type MarkdownElementProps = {
-  children: React.ReactNode
-}
-
-export async function generateMetadata(
-  { params }: PageProps,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const article = getArticleBySlug(params.slug)
   return {
-    title: 'Article',
+    title: article.title,
   }
 }
 
-export async function Article({ params }: PageProps) {
-  const rawMarkdown = readFileSync(`${writingsFolderPath}/${params.slug}.md`)
-  const syntaxHighlighter = await shiki.getHighlighter({ theme: 'dracula' })
-  const snippet = await unified()
-    .use(remarkParse)
-    .use(remarkCodeTitle)
-    .use(remarkShiki, { highlighter: syntaxHighlighter })
-    .use(remarkRehype, { allowDangerousHtml: true })
-    .use(rehypeRaw)
-    .use(rehypeSlug)
-    .use(rehypeStringify)
-    // @ts-expect-error I have no idea what's going on here.
-    .use(rehypeReact, {
-      createElement: React.createElement,
-      Fragment: React.Fragment,
-      components: {
-        h2: ({ children, ...props }: MarkdownElementProps) => (
-          <Heading {...props} level={2} as="h2">
-            {children}
-          </Heading>
-        ),
-        h3: ({ children, ...props }: MarkdownElementProps) => (
-          <Heading {...props} level={3} as="h3">
-            {children}
-          </Heading>
-        ),
-        h4: ({ children, ...props }: MarkdownElementProps) => (
-          <Heading {...props} level={4} as="h4">
-            {children}
-          </Heading>
-        ),
-        p: ({ children, ...props }: MarkdownElementProps) => (
-          <Text {...props} as="p" size="base">
-            {children}
-          </Text>
-        ),
-        li: ({ children, ...props }: MarkdownElementProps) => (
-          <Text {...props} as="li" size="base">
-            {children}
-          </Text>
-        ),
-        a: ({ children, ...props }: MarkdownElementProps) => (
-          // @ts-expect-error href will be injected from markdown
-          <Link {...props} target="_blank" rel="noopener">
-            {children}
-          </Link>
-        ),
-        strong: ({ children, ...props }: MarkdownElementProps) => (
-          <Text {...props} as="strong" size="base" color="primary" weight="bold">
-            {children}
-          </Text>
-        ),
-        img: ({ children, ...props }: MarkdownElementProps) => (
-          // @ts-expect-error src prop will be injected from markdown
-          <Image {...props} alt="" width={800} height={500} className="rounded-lg">
-            {children}
-          </Image>
-        ),
-      },
-    })
-    .process(rawMarkdown)
+export function Article({ params }: PageProps) {
+  const article = getArticleBySlug(params.slug)
 
   return (
     <React.Fragment>
-      <Navbar background={slateDark.slate2} />
+      <Navbar background={article.color} />
       <main>
-        <header className={styles.header}>
-          <div className={styles.headerContent}>
-            <Heading as="h1" level={1}>
-              The path of building my website on 2023
-            </Heading>
-            <Text as="time" size="large">
-              14 March, 2023 — 15 min
-            </Text>
-          </div>
-        </header>
-        <article className={styles.article}>{snippet.result}</article>
+        <ArticleHeader title={article.title} date={article.date} color={article.color} />
+        <article className={styles.article}>
+          <MDXToContent content={article.body.code} />
+        </article>
       </main>
     </React.Fragment>
   )
