d9bf4a2f7b28f6a02d09b6a72021ddff150027b5
emkis <nicolasemkis@gmail.com>
2023-07-23 18:45:22 +0100

    feat(tokens): provide different options to use registered theme

--- src/tokens/utilities/register-theme.ts
+++ src/tokens/utilities/register-theme.ts
@@ -1,7 +1,12 @@
 import type { Theme } from '../types'
 
-export type ThemeAttribute = { 'data-theme': Theme }
+export type ThemeAttributeProps = { 'data-theme': Theme }
 
-export function registerTheme(theme: Theme): ThemeAttribute {
-  return { 'data-theme': theme }
+export const themeAttribute = 'data-theme'
+
+export function registerTheme(theme: Theme) {
+  const props: ThemeAttributeProps = { [themeAttribute]: theme }
+  const selector = `[${themeAttribute}='${theme}']` as const
+
+  return { props, selector }
 }
