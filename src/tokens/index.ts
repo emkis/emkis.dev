4371a31234dc7bfbfa11b0eb2a5a8b4f15c0f469
emkis <nicolasemkis@gmail.com>
2023-08-03 20:21:30 +0100

    refactor(tokens): change exported type

--- src/tokens/index.ts
+++ src/tokens/index.ts
@@ -4,7 +4,7 @@ export type { Token, CSSVarToken } from './utilities/token'
 export type { Theme } from './types'
 
 export { registerTheme, themeAttribute } from './utilities/register-theme'
-export type { ThemeAttribute } from './utilities/register-theme'
+export type { ThemeAttributeProps } from './utilities/register-theme'
 
 export { breakpoint } from './utilities/breakpoint'
 export type { Breakpoint, CSSBreakpoint } from './utilities/breakpoint'
