ed9e583916daa8ea313cf4a824fa5571c0fc60e9
emkis <nicolasemkis@gmail.com>
2023-09-02 23:12:42 +0100

    feat(ui): add link tokens for Link component

--- src/ui/components/Link/styles.css.ts
+++ src/ui/components/Link/styles.css.ts
@@ -1,10 +1,15 @@
-import { style } from '@vanilla-extract/css'
+import { globalStyle, style } from '@vanilla-extract/css'
 import { token } from '@tokens'
 
 export const link = style({
   fontFamily: token('font-family.body'),
   cursor: 'pointer',
-  color: token('color.text-primary'),
+  color: token('color.text-link'),
   textDecoration: 'underline',
   textUnderlineOffset: '2px',
+  selectors: {
+    '&:visited': {
+      color: token('color.text-link-visited'),
+    },
+  },
 })
