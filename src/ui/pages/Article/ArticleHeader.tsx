f97ad22763eacc11f162331831870ab5a9ff2612
emkis <nicolasemkis@gmail.com>
2023-09-02 23:23:28 +0100

    refactor(ui): remove color prop from header

--- src/ui/pages/Article/ArticleHeader.tsx
+++ src/ui/pages/Article/ArticleHeader.tsx
@@ -4,7 +4,6 @@ import type * as React from 'react'
 import { format, parseISO } from 'date-fns'
 import { Heading } from '@ui/components/Heading'
 import { Text } from '@ui/components/Text'
-import { assignInlineVars } from '@vanilla-extract/dynamic'
 import { registerTheme } from '@tokens'
 import * as styles from './styles.css'
 
@@ -13,16 +12,14 @@ type NativeProps = Omit<JSX.IntrinsicElements['header'], 'ref'>
 export type ArticleHeaderProps = NativeProps & {
   title: string
   date: string
-  color: string
   readTime: number
 }
 
 export function ArticleHeader(props: ArticleHeaderProps) {
-  const { title, date, color, readTime } = props
-  const rootStyles = assignInlineVars({ [styles.articleBackground]: color })
+  const { title, date, readTime } = props
 
   return (
-    <header {...registerTheme('dark').props} style={rootStyles}>
+    <header {...registerTheme('dark').props}>
       <div className={styles.header}>
         <div className={styles.headerContent}>
           <Heading as="h1" level={1}>
