9705e63bc3066d186ad7160b65ea77362e040a11
emkis <nicolasemkis@gmail.com>
2025-09-10 13:11:14 +0100

    first typography attempt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,4 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -23,10 +11,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html lang="en" /* className="dark" */>
+      <body>{children}</body>
     </html>
   )
 }
