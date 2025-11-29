eb197be31e576ad221084bad4ee5e43777bf6219
emkis <nicolasemkis@gmail.com>
2025-11-29 22:36:39 +0000

    Removing that is not needed for the vanilla project

--- src/ui-core/utilities/classnames.ts
+++
@@ -1,9 +0,0 @@
-import { type ClassValue, clsx } from 'clsx'
-
-type ClassNameValue = ClassValue
-
-function cn(...classes: ClassValue[]) {
-  return clsx(classes)
-}
-
-export { cn, type ClassNameValue }
