98daa8cb504aa41fc42c174addfce6c4cc3bad80
emkis <nicolasemkis@gmail.com>
2025-12-09 16:55:12 +0000

    Update astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
-// @ts-check
-
 import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import { defineConfig } from 'astro/config'
-
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
