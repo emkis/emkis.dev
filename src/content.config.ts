bf994b82ac0bd9243bd683e53341419ffbd22f4e
emkis <nicolasemkis@gmail.com>
2025-12-09 17:14:40 +0000

    Update content configuration

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,16 +3,14 @@ import { glob } from 'astro/loaders'
 
 const blog = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+  loader: glob({ base: './writing', pattern: '**/*.{md,mdx}' }),
   // Type-check frontmatter using a schema
-  schema: ({ image }) =>
+  schema: (context) =>
     z.object({
       title: z.string(),
-      description: z.string(),
-      // Transform string to Date object
-      pubDate: z.coerce.date(),
-      updatedDate: z.coerce.date().optional(),
-      heroImage: image().optional(),
+      published_at: z.string().date(),
+      updated_at: z.string().date(),
+      featured: z.boolean().optional(),
     }),
 })
 
