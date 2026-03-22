2e5609b87d1eceaaf425fe1ff99bea8d862cf171
emkis <nicolasemkis@gmail.com>
2026-03-22 20:18:06 +0000

    Add getSortedArticlesByYear content function

--- src/content/articles.ts
+++ src/content/articles.ts
@@ -2,13 +2,24 @@ import { type CollectionEntry, getCollection } from 'astro:content'
 
 type Article = CollectionEntry<'writing'>
 
-async function getSortedArticles(): Promise<Article[]> {
-  const articles = await getCollection('writing')
-  return articles.sort(
-    (a, b) =>
-      new Date(b.data.published_at).valueOf() -
-      new Date(a.data.published_at).valueOf(),
+function sortDescendingPublishedDate(a: Article, b: Article): number {
+  return (
+    new Date(b.data.published_at).valueOf() -
+    new Date(a.data.published_at).valueOf()
   )
 }
 
-export { type Article, getSortedArticles }
+async function getSortedArticles(): Promise<Article[]> {
+  const articles = await getCollection('writing')
+  return articles.sort(sortDescendingPublishedDate)
+}
+
+async function getSortedArticlesByYear(): Promise<Map<number, Article[]>> {
+  const sortedArticles = await getSortedArticles()
+  return Map.groupBy(sortedArticles, (article) => {
+    const publishedAt = new Date(article.data.published_at)
+    return publishedAt.getFullYear()
+  })
+}
+
+export { type Article, getSortedArticles, getSortedArticlesByYear }
