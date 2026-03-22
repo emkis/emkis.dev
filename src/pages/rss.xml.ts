c370f95ecf29db3832f59661d2ca0cfdf61dc0ae
emkis <nicolasemkis@gmail.com>
2026-03-22 00:18:58 +0000

    Centralise site metadata

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,19 +1,22 @@
 import rss from '@astrojs/rss'
 import type { APIRoute } from 'astro'
 import { getOrderedArticles } from '@/content/get-ordered-articles'
+import { metadata } from '@/metadata'
 
 const GET: APIRoute = async (context) => {
   const articles = await getOrderedArticles()
 
   return rss({
-    title: 'Nicolas Jardim',
-    description:
-      'Articles about web development, software engineering, and technology.',
+    title: metadata.title,
+    description: metadata.description,
+    customData: '<language>en-us</language>',
     // biome-ignore lint/style/noNonNullAssertion: The site will be always defined
     site: context.site!,
     items: articles.map((post) => ({
-      ...post.data,
+      title: post.data.title,
+      description: post.data.description,
       link: `/writing/${post.id}/`,
+      pubDate: new Date(post.data.published_at),
     })),
   })
 }
